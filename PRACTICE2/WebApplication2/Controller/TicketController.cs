1e3f8807428184add7b3e085d0def71fc3da39ad
ahan1986 <andrew.a.han@gmail.com>
2018-11-09 09:26:48 -0500

    create put request

--- PRACTICE2/WebApplication2/Controller/TicketController.cs
+++ PRACTICE2/WebApplication2/Controller/TicketController.cs
@@ -58,5 +58,29 @@ namespace WebApplication2.Controller
             // createdAtRoute changes the route name of the information you want with "GetTicket" to hide it from the viewers. Then we are adding an id to the database, but here you can also add whatever you want here to the model blue print. Last argu ticket is what you're passing on to the one you just made such as object in json form.
             return CreatedAtRoute("GetTicket", new { id = ticket.Id }, ticket); 
         }
+
+        // get the Id of the one that we want to update from the database and then change it to TicketItem ticket from [FromBody]
+        public IActionResult Update(long Id, [FromBody] TicketItem ticket)
+        {
+            // if ticket doesnt exist or that ticket doenst match the id you asked for
+            if (ticket == null || ticket.Id != Id)
+            {
+                return BadRequest();
+            }
+            // going to get the ticket. Get the id of the id that yo have passed in.
+            var tic = _context.TicketItems.FirstOrDefault(t => t.Id == Id);
+            // make sure that ticket is not null but if it is say that its notfound
+            if(tic == null)
+            {
+                return NotFound();
+            }
+            tic.Concert = ticket.Concert;
+            tic.Available = ticket.Available;
+            tic.Artist = ticket.Artist;
+            _context.TicketItems.Update(tic);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
     }
 }
