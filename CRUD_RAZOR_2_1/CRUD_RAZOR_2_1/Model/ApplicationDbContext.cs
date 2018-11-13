359d351be987980dd32af01e90e1f5d43bc50e36
ahan1986 <andrew.a.han@gmail.com>
2018-11-13 11:51:16 -0500

    create basic constructor syntax in the model folder for the applicationDbContext to hook up with the startup database

---
+++ CRUD_RAZOR_2_1/CRUD_RAZOR_2_1/Model/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_RAZOR_2_1.Model
+{
+    //inherit/Extend DbContext from EntityFrameworkCore
+    public class ApplicationDbContext : DbContext
+    {
+        //need a constructor. type ctor and then tab twice for short cut to create constructor
+        // add in params the DbContextOptions from entityframworkcore and then the applicationdbContext in the params as the model
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
