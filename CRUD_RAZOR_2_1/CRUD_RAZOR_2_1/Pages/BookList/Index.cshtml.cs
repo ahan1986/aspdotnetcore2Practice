b9c938723eb5c2d4ee9230037cf569b0014b3fc0
ahan1986 <andrew.a.han@gmail.com>
2018-11-13 14:28:32 -0500

    add index.cshtml and it's model in the booklist folder and set up basic variables

---
+++ CRUD_RAZOR_2_1/CRUD_RAZOR_2_1/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_RAZOR_2_1.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_RAZOR_2_1.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        // to pass data from the page model to the razor page in a view in controller.  You can write property here. So in JS terms, define the variable here and then set the variable in the OnGet() method/handler.  This will now be able to send information out  to the respecitve razor page by typing @Model.someData like how you defined in here.
+        // public string someData { get; set; }
+
+        //to use the Book Model in our database, we need to acess our database by having the object of ApplicationDbContext here and defining a variable e.g. someData. dependency injection for CRUD_RAZOR_2_1.Model. Set this variable private so that no other classes can use it.
+        private readonly ApplicationDbContext _db;
+        // to retrieve the data, we create a list of books so we need IEnumerable
+        public IEnumerable<Book> Books { get; set; }
+        //create constructor. This is a boiler plate so that the local db can be used.
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        
+
+        // this model can only pass data to the View which will be the index.cshtml.cs in the BookList folder in the pages folder
+        public void OnGet() // this is a handler. This will be executed when a GET request comes in 
+        {
+            //someData = "This is the first property";
+
+        }
+    }
+}
