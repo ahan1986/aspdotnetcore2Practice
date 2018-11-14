d74e2988bb4cd310d1b1f55a5a0185af1b7ca2df
ahan1986 <andrew.a.han@gmail.com>
2018-11-14 13:37:23 -0500

    create message to user when they add a book

--- CRUD_RAZOR_2_1/CRUD_RAZOR_2_1/Pages/BookList/Create.cshtml.cs
+++ CRUD_RAZOR_2_1/CRUD_RAZOR_2_1/Pages/BookList/Create.cshtml.cs
@@ -11,6 +11,11 @@ namespace CRUD_RAZOR_2_1.Pages.BookList
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+
+        //tempData section from the tutorial.  This is to set a message to the user after they have created a book.
+        [TempData]
+        public string Message { get; set; }
+
         public CreateModel(ApplicationDbContext db)
         {
             _db = db;
@@ -33,6 +38,10 @@ namespace CRUD_RAZOR_2_1.Pages.BookList
             //you're grabbing Book defined in params and adding it to the database. Then you have to always SaveChangesAsync() this is saving the changes to the database. So you have to manually add it and then tell it to save it.
             _db.Books.Add(Book);
             await _db.SaveChangesAsync();
+
+            // this is fron the tempdata method created for a message to be sent to the user when they added a book.
+            Message = "Book has been created succesfully!";
+
             // Redirect to the Index section method. To Index page because thats where we will be displaying all the books.
             return RedirectToPage("Index");
         }
