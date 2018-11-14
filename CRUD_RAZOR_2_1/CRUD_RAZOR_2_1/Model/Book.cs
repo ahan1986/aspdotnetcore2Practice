68be8d24027a98ad6bb2872ba4dd3b4f3ce064ed
ahan1986 <andrew.a.han@gmail.com>
2018-11-14 11:43:10 -0500

    adding more comments

--- CRUD_RAZOR_2_1/CRUD_RAZOR_2_1/Model/Book.cs
+++ CRUD_RAZOR_2_1/CRUD_RAZOR_2_1/Model/Book.cs
@@ -11,5 +11,9 @@ namespace CRUD_RAZOR_2_1.Model
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Author { get; set; }
     }
 }
